8d9d8ca40faa8c6330857e571d0d1a1b56de5fbd
T N <truong1320vv@gmail.com>
2022-06-21 17:20:02 +0700

    add btc

--- stock_app.py
+++ stock_app.py
@@ -7,8 +7,9 @@ from dash.dependencies import Input, Output
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
-from datetime import date
+from datetime import datetime as date
 import yfinance as yf
+import binance as bi
 
 
 app = dash.Dash()
@@ -22,6 +23,7 @@ app.layout = html.Div([
         html.Div([
             dcc.Dropdown(id="stock-dropdown",
                          options=[{'label': 'Apple', 'value': 'AAPL'},
+                                  {'label': 'BTCUSDT', 'value': 'BTCUSDT'},
                                   {'label': 'Metaverse/Facebook', 'value': 'META'},
                                   {'label': 'Google', 'value': 'GOOG'},
                                   {'label': 'Netflix', 'value': 'NFLX'},
@@ -87,6 +89,10 @@ app.layout = html.Div([
 
 ])
 
+
+def filter(item: list):
+    return item[0:6]
+
 # app callback visualizes closing values
 
 
@@ -95,12 +101,38 @@ app.layout = html.Div([
     [Input("graph-type-dropdown", "value"),
      Input("stock-dropdown", "value"),
      Input("period-dropdown", "value"),
+     Input("interval-dropdown", "value"),
      Input("date-picker-range", "start_date"),
      Input("date-picker-range", "end_date")]
 )
-def update_graph(graph_type, stock, period, start_date, end_date):
-    yf_ticker_data = yf.Ticker(stock)
-    df = yf_ticker_data.history(period=period, start=start_date, end=end_date)
+def update_graph(graph_type, stock, period, interval, start_date, end_date):
+    if stock == "BTCUSDT":
+        tmp_start = int(
+            round(date.fromisoformat(start_date).timestamp())) * 1000
+        tmp_end = int(round(date.fromisoformat(end_date).timestamp())) * 1000
+        client = bi.Client()
+        res = client.get_klines(
+            symbol=stock,
+            interval=client.KLINE_INTERVAL_5MINUTE,
+            limit=1000,
+            startTime=tmp_start,
+            endTime=tmp_end)
+        fil = list(map(filter, res))
+        df = pd.DataFrame(
+            fil, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
+        df['Open'] = df['Open'].astype('float64')
+        df['High'] = df['High'].astype('float64')
+        df['Low'] = df['Low'].astype('float64')
+        df['Close'] = df['Close'].astype('float64')
+        df['Volume'] = df['Volume'].astype('float64')
+        df["Date"] = pd.to_datetime(df["Date"], unit='ms')
+        df.index = df["Date"]
+    else:
+        yf_ticker_data = yf.Ticker(stock)
+        df = yf_ticker_data.history(
+            period=period,
+            start=date.fromisoformat(start_date).strftime("%Y-%m-%d"),
+            end=date.fromisoformat(end_date).strftime("%Y-%m-%d"))
 
     match graph_type:
         case "Close":
